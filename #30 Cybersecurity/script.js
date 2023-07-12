103c9fec46d3f015f19ba23ed08c46487af6bd0d
Eduardo Reis Boattini <edureisboa@gmail.com>
2023-07-12 05:26:50 -0300

    Files Added
    Files Added.

--- #30 Cybersecurity/script.js
+++ #30 Cybersecurity/script.js
@@ -1,161 +1,29 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Cybersecurity Topics for White Hat Hackers</title>
-  <style>
-    /* CSS styles */
-    body {
-      font-family: Arial, sans-serif;
-      margin: 0;
-      padding: 0;
-    }
-    
-    header {
-      background-color: #333;
-      color: #fff;
-      padding: 20px;
-      text-align: center;
-    }
-    
-    h1 {
-      margin: 0;
-      font-size: 32px;
-    }
-    
-    nav {
-      background-color: #f5f5f5;
-      padding: 10px;
-      margin-bottom: 20px;
-    }
-    
-    nav ul {
-      list-style-type: none;
-      padding: 0;
-      margin: 0;
-    }
-    
-    nav ul li {
-      display: inline;
-    }
-    
-    nav ul li a {
-      color: #333;
-      text-decoration: none;
-      padding: 10px 20px;
-    }
-    
-    nav ul li a:hover {
-      background-color: #ddd;
-    }
-    
-    main {
-      margin: 20px;
-    }
-    
-    h2 {
-      margin-top: 0;
-    }
-  </style>
-</head>
-<body>
-  <header>
-    <h1>Cybersecurity Topics for White Hat Hackers</h1>
-  </header>
-  
-  <nav>
-    <ul>
-      <li><a href="#intro">1. Introduction to Cybersecurity</a></li>
-      <li><a href="#networking">2. Networking Fundamentals</a></li>
-      <li><a href="#os">3. Operating Systems Security</a></li>
-      <li><a href="#webapp">4. Web Application Security</a></li>
-      <li><a href="#coding">5. Secure Coding Practices</a></li>
-      <li><a href="#crypto">6. Cryptography</a></li>
-      <li><a href="#malware">7. Malware Analysis</a></li>
-      <li><a href="#networksec">8. Network Security</a></li>
-      <li><a href="#wireless">9. Wireless Network Security</a></li>
-      <li><a href="#cloud">10. Cloud Security</a></li>
-      <li><a href="#mobile">11. Mobile Security</a></li>
-      <li><a href="#incident">12. Incident Response</a></li>
-      <li><a href="#siem">13. Security Information and Event Management (SIEM)</a></li>
-      <li><a href="#pentest">14. Penetration Testing</a></li>
-      <li><a href="#vulnassess">15. Vulnerability Assessment</a></li>
-      <li><a href="#threatintel">16. Threat Intelligence</a></li>
-      <li><a href="#social">17. Social Engineering</a></li>
-      <li><a href="#iam">18. Identity and Access Management</a></li>
-      <li><a href="#audit">19. Security Auditing and Compliance</a></li>
-      <li><a href="#privacy">20. Data Privacy and Protection</a></li>
-      <li><a href="#netforensics">21. Network Forensics</a></li>
-      <li><a href="#digiforensics">22. Digital Forensics</a></
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Cybersecurity Topics for White Hat Hackers</title>
-</head>
-<body>
-  <header>
-    <h1>Cybersecurity Topics for White Hat Hackers</h1>
-  </header>
-  
-  <nav>
-    <ul>
-      <li><a href="#intro">1. Introduction to Cybersecurity</a></li>
-      <li><a href="#networking">2. Networking Fundamentals</a></li>
-      <li><a href="#os">3. Operating Systems Security</a></li>
-      <li><a href="#webapp">4. Web Application Security</a></li>
-      <li><a href="#coding">5. Secure Coding Practices</a></li>
-      <li><a href="#crypto">6. Cryptography</a></li>
-      <li><a href="#malware">7. Malware Analysis</a></li>
-      <li><a href="#networksec">8. Network Security</a></li>
-      <li><a href="#wireless">9. Wireless Network Security</a></li>
-      <li><a href="#cloud">10. Cloud Security</a></li>
-      <li><a href="#mobile">11. Mobile Security</a></li>
-      <li><a href="#incident">12. Incident Response</a></li>
-      <li><a href="#siem">13. Security Information and Event Management (SIEM)</a></li>
-      <li><a href="#pentest">14. Penetration Testing</a></li>
-      <li><a href="#vulnassess">15. Vulnerability Assessment</a></li>
-      <li><a href="#threatintel">16. Threat Intelligence</a></li>
-      <li><a href="#social">17. Social Engineering</a></li>
-      <li><a href="#iam">18. Identity and Access Management</a></li>
-      <li><a href="#audit">19. Security Auditing and Compliance</a></li>
-      <li><a href="#privacy">20. Data Privacy and Protection</a></li>
-      <li><a href="#netforensics">21. Network Forensics</a></li>
-      <li><a href="#digiforensics">22. Digital Forensics</a></
-      <li><a href="#threathunt">23. Cyber Threat Hunting</a></li>
-      <li><a href="#soc">24. Security Operations Center (SOC)</a></li>
-      <li><a href="#sdlc">25. Secure Software Development Life Cycle (SDLC)</a></li>
-      <li><a href="#reverse">26. Reverse Engineering</a></li>
-      <li><a href="#iot">27. IoT Security</a></li>
-      <li><a href="#ics">28. Industrial Control Systems (ICS) Security</a></li>
-      <li><a href="#virtualization">29. Virtualization and Container Security</a></li>
-      <li><a href="#ethicalhack">30. Ethical Hacking Methodologies</a></li>
-</ul>
+// JavaScript code for words animation when scrolling
 
-  </nav>
-  <main>
-  <section id="intro">
-      <h2>1. Introduction to Cybersecurity</h2>
-      <!-- Content for the introduction to cybersecurity topic -->
-      <p>An overview of the field of cybersecurity, its importance, common terminology, and the role of white hat hackers in protecting systems and networks.</p>
-    </section>
-    <section id="networking">
-  <h2>2. Networking Fundamentals</h2>
-  <!-- Content for the networking fundamentals topic -->
-  <p>An exploration of networking concepts, including TCP/IP, DNS, firewalls, routers, and network protocols relevant to cybersecurity.</p>
-</section>
+window.addEventListener('DOMContentLoaded', () => {
+  const animatedWords = document.querySelectorAll('.animated-word');
 
-<section id="os">
-  <h2>3. Operating Systems Security</h2>
-  <!-- Content for the operating systems security topic -->
-  <p>An examination of security measures for operating systems, including access controls, user management, hardening, and secure configurations.</p>
-</section>
+  function animateWords() {
+    animatedWords.forEach((word) => {
+      const wordTop = word.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
 
-<section id="webapp">
-  <h2>4. Web Application Security</h2>
-  <!-- Content for the web application security topic -->
-  <p>A focus on securing web applications by understanding common vulnerabilities, such as injection attacks, cross-site scripting (XSS), and session management.</p>
-</section>
+      if (wordTop < windowHeight * 0.8) {
+        const letters = word.textContent.split('');
+        word.textContent = '';
 
-<!-- Continue adding sections for each topic -->
-</main>
-</body>
-</html>
+        letters.forEach((letter, index) => {
+          const span = document.createElement('span');
+          span.textContent = letter;
+          span.style.animationDelay = `${index * 150}ms`;
+          word.append(span);
+        });
+
+       
+      }
+    });
+  }
+
+  window.addEventListener('scroll', animateWords);
+});
+
