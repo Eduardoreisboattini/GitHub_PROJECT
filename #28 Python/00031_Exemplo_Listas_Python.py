f80c2cc9f7d0d6a4cfe11dc84648a81104f48cba
Eduardo Reis Boattini <edureisboa@gmail.com>
2023-12-04 07:47:15 -0300

    Changed
    Changed

---
+++ #28 Python/00031_Exemplo_Listas_Python.py
@@ -0,0 +1,57 @@
+import random
+import numpy as np
+from scipy.stats import mode
+
+# Gerar uma lista de 20 números aleatórios
+dados = [random.randint(1, 100) for _ in range(20)]
+
+# 1. Média:
+media_dados = np.mean(dados)
+
+# 2. Mediana:
+mediana_dados = np.median(dados)
+
+# 3. Moda:
+moda_dados = mode(dados).mode[0]
+
+# 4. Desvio Padrão:
+desvio_padrao_dados = np.std(dados)
+
+# 5. Variância:
+variancia_dados = np.var(dados)
+
+# 6. Amplitude:
+amplitude_dados = np.ptp(dados)
+
+# 7. Quartis:
+q1 = np.percentile(dados, 25)
+q3 = np.percentile(dados, 75)
+
+# 8. Amplitude Interquartil (IQR):
+iqr = q3 - q1
+
+# Operações adicionais:
+# 9. Ordenação:
+dados_ordenados = sorted(dados)
+
+# 10. Filtragem (exemplo: números pares):
+dados_pares = [x for x in dados if x % 2 == 0]
+
+# 11. Iteração:
+for numero in dados:
+    print("Número:", numero)
+
+# Exibir o resumo estatístico
+print("\nResumo Estatístico:")
+print("Lista de Números Aleatórios:", dados)
+print("Média:", media_dados)
+print("Mediana:", mediana_dados)
+print("Moda:", moda_dados)
+print("Desvio Padrão:", desvio_padrao_dados)
+print("Variância:", variancia_dados)
+print("Amplitude:", amplitude_dados)
+print("Quartis - Q1:", q1, "Q3:", q3)
+print("Amplitude Interquartil (IQR):", iqr)
+print("\nOperações Adicionais:")
+print("Dados Ordenados:", dados_ordenados)
+print("Números Pares:", dados_pares)
