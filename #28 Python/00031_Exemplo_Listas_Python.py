dd3288e8e50a7432830785f2f16cdf0ec8c4d149
Eduardo Reis Boattini <edureisboa@gmail.com>
2023-12-04 07:43:30 -0300

    Code Changed
    Code

--- #28 Python/00031_Exemplo_Listas_Python.py
+++ #28 Python/00031_Exemplo_Listas_Python.py
@@ -1,42 +1,60 @@
+Certamente! Vamos adicionar operações de ordenação, filtragem e iteração à lista de dados no código. Aqui está o código atualizado:
+
+```python
 import random
 import numpy as np
 from scipy.stats import mode
 
-# Generate a list of 20 random numbers
-data = [random.randint(1, 100) for _ in range(20)]
+# Gerar uma lista de 20 números aleatórios
+dados = [random.randint(1, 100) for _ in range(20)]
 
-# 1. Mean:
-data_mean = np.mean(data)
+# 1. Média:
+media_dados = np.mean(dados)
 
-# 2. Median:
-data_median = np.median(data)
+# 2. Mediana:
+mediana_dados = np.median(dados)
 
-# 3. Mode:
-data_mode = mode(data).mode[0]
+# 3. Moda:
+moda_dados = mode(dados).mode[0]
 
-# 4. Standard Deviation:
-data_std = np.std(data)
+# 4. Desvio Padrão:
+desvio_padrao_dados = np.std(dados)
 
-# 5. Variance:
-data_var = np.var(data)
+# 5. Variância:
+variancia_dados = np.var(dados)
 
-# 6. Range:
-data_range = np.ptp(data)
+# 6. Amplitude:
+amplitude_dados = np.ptp(dados)
 
-# 7. Quartiles:
-q1 = np.percentile(data, 25)
-q3 = np.percentile(data, 75)
+# 7. Quartis:
+q1 = np.percentile(dados, 25)
+q3 = np.percentile(dados, 75)
 
-# 8. Interquartile Range (IQR):
+# 8. Amplitude Interquartil (IQR):
 iqr = q3 - q1
 
-# Display the statistical summary
-print("List of Random Numbers:", data)
-print("Mean:", data_mean)
-print("Median:", data_median)
-print("Mode:", data_mode)
-print("Standard Deviation:", data_std)
-print("Variance:", data_var)
-print("Range:", data_range)
-print("Quartiles - Q1:", q1, "Q3:", q3)
-print("Interquartile Range (IQR):", iqr)
+# Operações adicionais:
+# 9. Ordenação:
+dados_ordenados = sorted(dados)
+
+# 10. Filtragem (exemplo: números pares):
+dados_pares = [x for x in dados if x % 2 == 0]
+
+# 11. Iteração:
+for numero in dados:
+    print("Número:", numero)
+
+# Exibir o resumo estatístico
+print("\nResumo Estatístico:")
+print("Lista de Números Aleatórios:", dados)
+print("Média:", media_dados)
+print("Mediana:", mediana_dados)
+print("Moda:", moda_dados)
+print("Desvio Padrão:", desvio_padrao_dados)
+print("Variância:", variancia_dados)
+print("Amplitude:", amplitude_dados)
+print("Quartis - Q1:", q1, "Q3:", q3)
+print("Amplitude Interquartil (IQR):", iqr)
+print("\nOperações Adicionais:")
+print("Dados Ordenados:", dados_ordenados)
+print("Números Pares:", dados_pares)
