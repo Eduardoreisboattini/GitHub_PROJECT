2fe6e8bf8cb7ded1860ea356585ea604e7b5b06e
Eduardo Reis Boattini <edureisboa@gmail.com>
2024-08-20 11:10:48 -0300

    Files created.
    Files Created.

---
+++ #28 Python/00070_Dash.py
@@ -0,0 +1,7 @@
+html.Div([
+    html.H1('Hello Dash'),
+    html.Div([
+        html.P('Dash converts Python classes into HTML'),
+        html.P("This conversion happens behind the scenes by Dash's JavaScript front-end")
+    ])
+])
---
+++ #28 Python/00071_Dash 01.py
@@ -0,0 +1,7 @@
+<div>
+    <h1>Hello Dash</h1>
+    <div>
+        <p>Dash converts Python classes into HTML</p>
+        <p>This conversion happens behind the scenes by Dash's JavaScript front-end</p>
+    </div>
+</div>
